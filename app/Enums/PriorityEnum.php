fa4e21751ac72ee7aa9c5b84ff6a6757ee254507
yefrem <volodymyr.yefremov@get-code.net>
2025-06-18 10:17:58 +0300

    fix Enum type

--- app/Enums/PriorityEnum.php
+++ app/Enums/PriorityEnum.php
@@ -4,11 +4,22 @@ declare(strict_types = 1);
 
 namespace App\Enums;
 
-enum PriorityEnum: string
+enum PriorityEnum: int
 {
-    case HIGH = '1';
-    case MIDHIGH = '2';
-    case MID = '3';
-    case MIDLOW = '4';
-    case LOW = '5';
+    case HIGH = 1;
+    case MIDHIGH = 2;
+    case MID = 3;
+    case MIDLOW = 4;
+    case LOW = 5;
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::HIGH => 'High',
+            self::MIDHIGH => 'Mid-High',
+            self::MID => 'Medium',
+            self::MIDLOW => 'Mid-Low',
+            self::LOW => 'Low',
+        };
+    }
 }

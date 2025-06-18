6fd184ecf5ea73f33520416f9598234babcf8e7c
yefrem <volodymyr.yefremov@get-code.net>
2025-06-18 18:29:12 +0300

    adjust Enums

--- app/Data/TaskCreateData.php
+++ app/Data/TaskCreateData.php
@@ -6,7 +6,10 @@ namespace App\Data;
 
 use App\Enums\PriorityEnum;
 use App\Enums\StatusEnum;
-use Illuminate\Validation\Rules\Enum;
+use Carbon\Carbon;
+use Illuminate\Validation\Rules\Enum as EnumRule;
+use Spatie\LaravelData\Attributes\WithCast;
+use Spatie\LaravelData\Casts\DateTimeInterfaceCast;
 use Spatie\LaravelData\Data;
 
 class TaskCreateData extends Data
@@ -18,6 +21,7 @@ class TaskCreateData extends Data
         public StatusEnum $status = StatusEnum::TODO,
         public PriorityEnum $priority = PriorityEnum::LOW,
         public ?int $parentId = null,
+        public ?\DateTimeInterface $due_date = null,
     ) {}
 
     public static function rules(): array
@@ -26,9 +30,10 @@ class TaskCreateData extends Data
             'userId' => ['required', 'integer', 'exists:users,id'],
             'title' => ['required', 'string', 'max:255'],
             'description' => ['nullable', 'string'],
-            'status' => ['nullable', new Enum(StatusEnum::class)],
-            'priority' => ['nullable', new Enum(PriorityEnum::class)],
+            'status' => ['nullable', new EnumRule(StatusEnum::class)],
+            'priority' => ['nullable', new EnumRule(PriorityEnum::class)],
             'parentId' => ['nullable', 'exists:tasks,id'],
+            'due_date' => ['nullable', 'date', 'after_or_equal:today'],
         ];
     }
 }
--- app/Data/TaskUpdateData.php
+++ app/Data/TaskUpdateData.php
@@ -14,9 +14,11 @@ class TaskUpdateData extends Data
     public function __construct(
         public ?string $title = null,
         public ?string $description = null,
-        public ?PriorityEnum $priority = null,
         public ?StatusEnum $status = null,
+        public ?PriorityEnum $priority = null,
         public ?int $parentId = null,
+        public ?\DateTimeInterface $due_date = null,
+        public ?\DateTimeInterface $completed_at = null,
     ) {}
 
     public static function rules(): array
@@ -27,6 +29,8 @@ class TaskUpdateData extends Data
             'status' => ['nullable', new Enum(StatusEnum::class)],
             'priority' => ['nullable', new Enum(PriorityEnum::class)],
             'parentId' => ['nullable', 'exists:tasks,id'],
+            'due_date' => ['nullable', 'date'],
+            'completed_at' => ['nullable', 'date', 'before_or_equal:now'],
         ];
     }
 }

2cfc2a33133027f7b2f4c110ea6e230c71b9cb40
yefrem <volodymyr.yefremov@get-code.net>
2025-06-23 15:46:20 +0300

    refactoring TaskSortingData.php (and rename)

---
+++ app/Data/TaskSortingData.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Data;
+
+use App\Enums\TaskSortFieldEnum;
+use Spatie\LaravelData\Data;
+
+class TaskSortingData extends Data
+{
+    /**
+     * @param array<array{field: TaskSortFieldEnum, direction: string}> $sorts
+     */
+    public function __construct(
+        public array $sorts = []
+    ) {}
+
+    public static function fromString(?string $sortString): self
+    {
+        if (empty($sortString)) {
+            return new self();
+        }
+
+        $sorts = [];
+        $sortParts = explode(',', $sortString);
+
+        foreach ($sortParts as $part) {
+            [$field, $direction] = array_pad(explode(':', $part), 2, 'asc');
+
+            if (!in_array($field, TaskSortFieldEnum::values())) {
+                continue;
+            }
+
+            if (!in_array(strtolower($direction), ['asc', 'desc'])) {
+                $direction = 'asc';
+            }
+
+            $sorts[] = [
+                'field' => TaskSortFieldEnum::from($field),
+                'direction' => strtolower($direction),
+            ];
+        }
+
+        return new self($sorts);
+    }
+}

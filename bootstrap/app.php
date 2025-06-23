8a6b6f2daf9e2c2a9a4697dc1243e99a79e5ad28
yefrem <volodymyr.yefremov@get-code.net>
2025-06-23 16:27:18 +0300

    add the custom exception

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Exceptions\TaskOperationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,9 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
+    ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->renderable(function (TaskOperationException $e, Request $request) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 422);
+        });
     })->create();

df1bc881eb07dcedf16083469309d9fd8ac89b30
yefrem <volodymyr.yefremov@get-code.net>
2025-06-23 22:34:09 +0300

    create TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,14 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use \Tests\CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->withoutVite();
+        $this->withHeaders([
+            'Accept' => 'application/json'
+        ]);
+    }
 }

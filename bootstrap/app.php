1a18c0801196033cbc2b9016b2d3cd3922cd0fdf
yefrem <volodymyr.yefremov@get-code.net>
2025-06-23 22:33:01 +0300

    adjust bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,10 @@
 use App\Exceptions\TaskOperationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +15,17 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->api([
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ]);
+
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+    })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->renderable(function (TaskOperationException $e, Request $request) {
             return response()->json([

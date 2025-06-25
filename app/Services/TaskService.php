e418d035ec9abb98413ffa311de90c315690d4af
yefrem <volodymyr.yefremov@get-code.net>
2025-06-25 13:52:18 +0300

    fix model type inconvenience

--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -11,6 +11,7 @@ use App\Data\TaskUpdateData;
 use App\Enums\StatusEnum;
 use App\Exceptions\TaskOperationException;
 use App\Models\Task;
+use App\Models\User;
 use App\Repositories\TaskRepository;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Auth;
@@ -22,37 +23,45 @@ readonly class TaskService
         private TaskRepository $repository
     ) {}
 
+    private function getAuthenticatedUser(): User
+    {
+        /** @var User $user */
+        $user = Auth::user();
+
+        return $user;
+    }
+
     public function getFiltered(?TaskFiltersData $filters, ?TaskSortingData $sort): Collection
     {
-        $user = Auth::user();
+        $user = $this->getAuthenticatedUser();
 
         return $this->repository->getByFiltersAndSort($user, $filters, $sort);
     }
 
     public function create(TaskCreateData $data): Task
     {
-        $user = Auth::user();
+        $user = $this->getAuthenticatedUser();
 
         return DB::transaction(fn () => $this->repository->create($user, $data));
     }
 
     public function update(int $taskId, TaskUpdateData $data): Task
     {
-        $user = Auth::user();
+        $user = $this->getAuthenticatedUser();
 
         return DB::transaction(fn () => $this->repository->update($user, $taskId, $data));
     }
 
     public function findById(int $id): Task
     {
-        $user = Auth::user();
+        $user = $this->getAuthenticatedUser();
 
         return $this->repository->findById($user, $id);
     }
 
     public function delete(int $taskId): void
     {
-        $user = Auth::user();
+        $user = $this->getAuthenticatedUser();
 
         DB::transaction(function () use ($user, $taskId) {
             $task = $this->repository->findById($user, $taskId);
@@ -67,7 +76,7 @@ readonly class TaskService
 
     public function markAsComplete(int $taskId): Task
     {
-        $user = Auth::user();
+        $user = $this->getAuthenticatedUser();
 
         return $this->repository->markAsComplete($user, $taskId);
     }
